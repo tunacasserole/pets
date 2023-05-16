db023e40d0e0c8ad20287a7c6a9d23f22432477c
Aaron Henderson <aaron@mashbot.com>
2023-05-16 13:34:19 -0600

    install quicksand font and sass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.sass.inline_source_maps = true
+  config.sass.line_comments = false
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
