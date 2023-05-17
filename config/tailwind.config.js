5a6aaf339d7f3968b11196e0fca0a8caa2a9652f
Aaron Henderson <aaron@mashbot.com>
2023-05-17 09:43:28 -0600

    mobile menu

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["QuickSand", ...defaultTheme.fontFamily.sans],
+        sans: ["Quicksand", ...defaultTheme.fontFamily.sans],
       },
     },
   },
